638eef319307a12aa4e5cef03b802d75804f17bc
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2026-05-05 15:01:12 +0800

    Delete templates directory

--- templates/hono/index.ts
+++
@@ -1,4 +0,0 @@
-import { Hono } from 'hono';
-const app = new Hono();
-app.get('/', (c) => c.json({ message: 'Exocore Hono', status: 'running' }));
-export default { port: process.env.PORT || 3000, fetch: app.fetch };
--- templates/nuxtjs/nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtConfig({
-  devtools: { enabled: false },
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-})
