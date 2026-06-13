dd296993d40529a7bc68c63e7ddd5d328512135f
Johnsteve Costanos <johnstevegamer5@gmail.com>
2026-06-13 10:56:33 +0800

    clean: remove legacy exocore-web files, keep only standards docs

--- dist/templates/cli-tool/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-const args = process.argv.slice(2);
-console.log('CLI Tool args:', args);
--- dist/templates/love2d/main.lua
+++
@@ -1,3 +0,0 @@
-function love.draw()
-    love.graphics.print('Hello LÖVE!', 100, 100)
-end
